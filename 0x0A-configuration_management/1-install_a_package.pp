2a63a488c8b92c679050cf1ec9cce7b86450a43d
Fatima Zohra <101020848+26fati@users.noreply.github.com>
2023-11-24 19:19:17 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
-# Install flask version 2.1.0 using pip3.
+# Using Puppet, install flask from pip3
 
 package { 'flask':
-ensure => '2.1.0',
-provider => 'pip3',
-}
-
-# Ensure Werkzeug version 2.1.1
-
-package { 'Werkzeug':
-ensure => '2.1.1',
-provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
